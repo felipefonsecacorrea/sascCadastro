b124d9d64fe2ecfffcf128c2a8fe05accf97abae
Felipe Fonseca Correa <felipefcfcfc@gmail.com>
2024-02-16 20:47:38 -0300

    bosta toda 3

--- src/main.tsx
+++ src/main.tsx
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       <Routes>
 
         <Route
-          path="cadastro"
+          path="/"
           element={
               
               <Cadastro />
